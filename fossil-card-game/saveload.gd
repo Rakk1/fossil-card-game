d611e0136eafeaad990a51661364e521d0487af1
Rieke123 <riekejansen@hotmail.nl>
2026-08-23 15:15:17 +0200

    saveload setup

---
+++ fossil-card-game/saveload.gd
@@ -0,0 +1,42 @@
+extends Node
+
+var latestversion = 0
+const savelocation = "user://savefile.json"
+
+var savecontents = {
+	"version": 0,
+	"savedDinos": {},
+	"settings": {
+		"globalvolume": 1,
+		"musicvolume": 1,
+		"font": "regular",
+		"fontsize": 16
+	}
+}
+
+
+func _ready() -> void:
+	_load()
+
+func _save():
+	var file = FileAccess.open(savelocation, FileAccess.WRITE)
+	file.store_var(savecontents.duplicate())
+	file.close()
+
+func _load():
+	if FileAccess.file_exists(savelocation):
+		var file = FileAccess.open(savelocation, FileAccess.READ)
+		var data = file.get_var()
+		file.close()
+		
+		var savedata = data.duplicate()
+		if(not savedata.has("version")):
+			savedata["version"] = 0
+		while(savedata.version != latestversion):
+			savedata = getHigherVersion(savedata)
+		savecontents = savedata
+
+func getHigherVersion(savedata):
+	match savedata.version:
+		0:
+			pass
